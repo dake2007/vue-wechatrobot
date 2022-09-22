8f3456d3fb8c184c507f57d1b40e7705c54a80f9
dake2007 <pdskill@163.com>
2022-09-22 22:23:24 +0800

    点击事件

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
-
-    <!-- 一级路由规则对象组件的挂载点 -->
-    <router-view></router-view>
-
+  <!-- 一级路由规则对象组件的挂载点 -->
+  <router-view></router-view>
 </template>
 
 <script>
 import { registerAPI } from './api'
 export default {
   async created () {
-    const res = await registerAPI()
-    console.log(res)
+    await registerAPI()
+    // console.log(res)
   }
 }
 </script>
--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -6,13 +6,75 @@
       <!-- 标题的盒子 -->
       <div class="title-box"></div>
       <!-- 注册的表单区域 -->
+      <el-form :model="form" ref="form" :rules="rulesObj">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input type="password" v-model="form.password" placeholder="请输入密码"></el-input>
+        </el-form-item>
+        <!-- 确认密码 -->
+        <el-form-item prop="repassword">
+          <el-input type="password" v-model="form.repassword" placeholder="请再次确认密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="btn-reg" type="primary" @click="registerFn">注册</el-button>
+          <el-link type="info">去登录</el-link>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'my-register'
+  name: 'my-register',
+  data () {
+    const samePwdFn = (rule, value, callback) => {
+      if (value !== this.form.password) {
+        callback(new Error('两次输入的密码不匹配'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      form: {
+        // 表单的数据对象
+        username: '',
+        password: '',
+        repassword: ''
+      },
+      rulesObj: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { pattern: /^[\S]{6,12}$/, message: '密码必须是6到12位', trigger: 'blur' }
+        ],
+        repassword: [
+          { required: true, message: '请再次输入密码', trigger: 'blur' },
+          { pattern: /^[\S]{6,12}$/, message: '密码必须是6到12位', trigger: 'blur' },
+          { validator: samePwdFn, trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    registerFn () {
+      // js兜底校验
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          console.log(this.form)
+        } else {
+          return false // 阻止默认提交行为(表单下面红色提示会自动弹出)
+        }
+      })
+    }
+  }
 }
 </script>
 
