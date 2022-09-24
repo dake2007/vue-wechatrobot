47ebdeed650fb725a04c611ee4b33ece16870fe9
dake2007 <pdskill@163.com>
2022-09-24 23:46:31 +0800

    配置添加对话框重置表单

--- src/views/article/artCate.vue
+++ src/views/article/artCate.vue
@@ -19,8 +19,15 @@
       </template>
     </el-card>
     <!-- 提示对话框 -->
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
-      <span>这是一段信息</span>
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" @closed="resetcloseFn">
+      <el-form :model="addForm" :rules="addRules" ref="addRef" label-width="80px">
+        <el-form-item label="分类名称" prop="cate_name">
+          <el-input v-model="addForm.cate_name" minlength="1" maxlength="10"></el-input>
+        </el-form-item>
+        <el-form-item label="分类别名" prop="cate_alias">
+          <el-input v-model="addForm.cate_alias" minlength="1" maxlength="15"></el-input>
+        </el-form-item>
+      </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="canceFn">取 消</el-button>
         <el-button type="primary" @click="confirmFn">确 定</el-button>
@@ -36,7 +43,23 @@ export default {
   data () {
     return {
       cateList: [],
-      dialogVisible: false
+      dialogVisible: false,
+      addForm: {
+        // 添加表单的数据对象
+        cate_name: '',
+        cate_alias: ''
+      },
+      addRules: {
+        // 添加表单的验证规则对象
+        cate_name: [
+          { required: true, message: '请输入分类名称', trigger: 'blur' },
+          { pattern: /\p{Unified_Ideograph}/u, message: '分类名必须是中文', trigger: 'blur' }
+        ],
+        cate_alias: [
+          { required: true, message: '请输入分类别名', trigger: 'blur' },
+          { pattern: /^[a-zA-Z0-9]{1,15}$/, message: '分类别名必须是1-15位的字母数字', trigger: 'blur' }
+        ]
+      }
     }
   },
   created () {
@@ -56,6 +79,9 @@ export default {
     },
     confirmFn () {
       this.dialogVisible = false
+    },
+    resetcloseFn () {
+      this.$refs.addRef.resetFields()
     }
   }
 }
