4b856d017f10907396d8583a42e0c835431efc5c
dake2007 <pdskill@163.com>
2022-09-26 12:39:25 +0800

    就是上传

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import store from './store'
 import '@/assets/global.less'
 import '@/elementUI/index'
 
+import VueQuillEditor from 'vue-quill-editor'
+// require styles
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+Vue.use(VueQuillEditor)// 此方法内会用vue.component注册quillEditor组件
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/article/artList.vue
+++ src/views/article/artList.vue
@@ -9,8 +9,7 @@
         <el-form :inline="true" :model="q">
           <el-form-item label="文章分类">
             <el-select v-model="q.cate_id" placeholder="请选择分类" size="small">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
+              <el-option v-for="obj in cateList" :key="obj.Id" :label="obj.name" :value="obj.Id"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="发布状态" style="margin-left: 15px;">
@@ -25,9 +24,25 @@
           </el-form-item>
         </el-form>
         <!-- 发表文章的按钮 -->
-        <el-button type="primary" size="small" class="btn-pub">发表文章</el-button>
+        <el-button type="primary" size="small" class="btn-pub" @click="showPubdialogFn">发表文章</el-button>
       </div>
-
+      <!-- 发表文章的 Dialog 对话框 -->
+      <el-dialog title="创建关键词" :visible.sync="pubDialogVisible" fullscreen :before-close="handleClose">
+        <!-- 发布文章的对话框 -->
+        <el-form :model="pubForm" :rules="pubFormRules" ref="pubFormRef" label-width="100px">
+          <el-form-item label="文章标题" prop="title">
+            <el-input v-model="pubForm.title" placeholder="请输入标题"></el-input>
+          </el-form-item>
+          <el-form-item label="文章分类" prop="cate_id">
+            <el-select v-model="pubForm.cate_id" placeholder="请选择分类" style="width: 100%;">
+              <el-option v-for="obj in cateList" :key="obj.Id" :label="obj.name" :value="obj.Id"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item lable="文章的内容" prop="content">
+            <quill-editor :options="editorOption" v-model="pubForm.content"></quill-editor>
+          </el-form-item>
+        </el-form>
+      </el-dialog>
       <!-- 文章表格区域 -->
 
       <!-- 分页区域 -->
@@ -36,17 +51,81 @@
 </template>
 
 <script>
+import { getArticleAPI } from '@/api'
+import 'quill/dist/quill.snow.css'
 export default {
   name: 'ArtList',
   data () {
     return {
+      editorOption: {
+        modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'], // 加粗 斜体 下划线 删除线
+            ['blockquote', 'code-block'], // 引用  代码块
+            [{ header: 1 }, { header: 2 }], // 1、2 级标题
+            [{ list: 'ordered' }, { list: 'bullet' }], // 有序、无序列表
+            [{ script: 'sub' }, { script: 'super' }], // 上标/下标
+            [{ indent: '-1' }, { indent: '+1' }], // 缩进
+            [{ direction: 'rtl' }], // 文本方向
+            [{ size: ['12', false, '16', '18', '20', '24', '28', '32', '36'] }], // 字体大小
+            [{ header: [1, 2, 3, 4, 5, 6] }], // 标题
+            [{ color: [] }, { background: [] }], // 字体颜色、字体背景颜色
+            // [{ font: ['songti'] }], // 字体种类
+            [{ align: [] }], // 对齐方式
+            ['clean'], // 清除文本格式
+            ['image', 'video'] // 链接、图片、视频
+          ]
+        },
+        placeholder: '请输入正文'
+      },
+      cateList: [], // 保存文章分类列表
       // 查询参数对象
       q: {
         pagenum: 1,
         pagesize: 2,
         cate_id: '',
         state: ''
+      },
+      pubDialogVisible: false,
+      pubForm: {
+        // 表单的数据对象 发布
+        title: '',
+        cate_id: '',
+        content: '' // 文章的内容
+      },
+      pubFormRules: {
+        // 表单的验证规则对象 发布
+        title: [
+          { required: true, message: '请输入文章标题', trigger: 'blur' },
+          { min: 1, max: 30, message: '文章标题的长度为1-30个字符', trigger: 'blur' }
+        ],
+        cate_id: [{ required: true, message: '请选择文章标题', trigger: 'blur' }],
+        content: [{ required: true, message: '请输入文章内容', trigger: 'blur' }]
       }
+    }
+  },
+  created () {
+    this.getCateListFn()
+  },
+  methods: {
+    async getCateListFn () {
+      const { data: res } = await getArticleAPI()
+      this.cateList = res.data
+    },
+    showPubdialogFn () {
+      this.pubDialogVisible = true
+    },
+    async handleClose (done) {
+      const confirmResult = await this.$confirm('此操作将导致配置信息丢失, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+
+      // 取消了关闭-阻止住, 什么都不干
+      if (confirmResult === 'cancel') return
+      // 确认关闭
+      done()
     }
   }
 }
@@ -60,5 +139,8 @@ export default {
   .btn-pub {
     margin-top: 5px;
   }
+}
+::v-deep .ql-editor {
+  min-height: 300px;
 }
 </style>
